1a2f7bc7ffe24304ddd0a479546f6b2246ab3ae8
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-05-08 10:06:53 +0900

    Update test fixture

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 var licenselint = require("./lib")
 var fixture = JSON.stringify([
   {
-    "name": "read-package-json",
-    "license": "ISC",
-    "version": "1.3.3"
-  },
-  {
     "name": "commander",
     "license": "MIT",
     "version": "2.8.1"
+  },
+  {
+    "name": "read-package-json",
+    "license": "ISC",
+    "version": "1.3.3"
   }
 ], 2, 2)
 
